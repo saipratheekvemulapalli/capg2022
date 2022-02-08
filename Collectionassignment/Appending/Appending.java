3d1c8bb7823198bb77365c6131ed421418f72333
saipratheekvemulapalli <saipratheekvemulapalli@gmail.com>
2022-02-08 12:06:03 +0530

    Collection Assignment

---
+++ Collectionassignment/Appending/Appending.java
@@ -0,0 +1,18 @@
+package package01;
+import java.util.LinkedList;
+
+public class Appending {
+
+	public static void main(String[] args) {
+		 LinkedList<String> l_list = new LinkedList<String>();
+		 l_list.add("Red");
+         l_list.add("Green");
+         l_list.add("Black");
+         l_list.add("White");
+         l_list.add("Pink");
+         l_list.add("Yellow");
+         System.out.println("The linked list: " + l_list);
+
+	}
+
+}
---
+++ Collectionassignment/DupliEle/DupliEle.java
@@ -0,0 +1,21 @@
+package package01;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+public class DupliEle {
+
+	public static void main(String[] args) {
+		List<String> l = new ArrayList<String>();  
+        l.add("Mango");  
+        l.add("Banana");  
+        l.add("Mango");  
+        l.add("Apple");  
+        System.out.println(l.toString());  
+        Set<String> s = new LinkedHashSet<String>(l);  
+        System.out.println(s);  
+
+	}
+
+}
