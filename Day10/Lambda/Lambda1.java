61aa7be034a92c0c2ab9f06b3328faf3fb1b5938
saipratheekvemulapalli <saipratheekvemulapalli@gmail.com>
2022-02-10 12:20:47 +0530

    Lambda1

---
+++ Day10/Lambda/Lambda1.java
@@ -0,0 +1,34 @@
+package com.Day10;
+
+/*
+ * Following are the important characteristics of a lambda expression.
+
+Optional type declaration − No need to declare the type of a parameter. The compiler can inference the same from the value of the parameter.
+
+Optional parenthesis around parameter − No need to declare a single parameter in parenthesis.
+ For multiple parameters, parentheses are required.
+
+Optional curly braces − No need to use curly braces in expression body if the body contains a single statement.
+
+Optional return keyword − The compiler automatically returns the value if the body has a single expression to return the value.
+ Curly braces are required to indicate that expression returns a value.
+ * 
+ * *
+ */
+@FunctionalInterface
+interface Addable {
+
+	int add(int a, int ab);
+
+}
+
+public class Lambda1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Addable adddemo = (a,b)->(a+b);  //no return type	
+		System.out.println(adddemo.add(15, 5));
+		
+	}
+
+}
